6a3e3af595342746451138c78fc09d119e726370
sahachai <inyourtimeguy@gmail.com>
2021-06-12 13:22:05 +0700

    add session and logout

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, ValidationError
+from wtforms.validators import DataRequired, Length, ValidationError, EqualTo
 from flask_login import (
     LoginManager,
     UserMixin,
@@ -27,13 +26,9 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-# init user login
-login_manager = LoginManager()
-login_manager.init_app(app)
-
 
 # User model
-class User(UserMixin, db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
@@ -42,7 +37,7 @@ class User(UserMixin, db.Model):
 # Register Form
 class RegisterForm(FlaskForm):
     username = StringField(
-        "username",
+        "Create your username",
         validators=[
             DataRequired(),
             Length(
@@ -51,13 +46,20 @@ class RegisterForm(FlaskForm):
         ],
     )
     password = PasswordField(
-        "password",
+        "Create your password",
         validators=[
             DataRequired(),
             Length(
                 min=6, max=20, message="A password must be between 6 to 20 characters"
             ),
         ],
+    )
+    confirm = PasswordField(
+        "Confirm your password",
+        validators=[
+            DataRequired(),
+            EqualTo('password', message='Password is not match')
+        ]
     )
     submit = SubmitField("Sign up")
 
@@ -69,16 +71,9 @@ class RegisterForm(FlaskForm):
 
 # Login Form
 class LoginForm(FlaskForm):
-    username = StringField("username", validators=[DataRequired()])
-    password = PasswordField("password", validators=[DataRequired()])
+    username = StringField("username", validators=[DataRequired()], render_kw={"placeholder": "Username"})
+    password = PasswordField("password", validators=[DataRequired()], render_kw={"placeholder": "Password"})
     submit = SubmitField("Login")
-
-
-
-# load user
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 
 # register route
@@ -88,12 +83,12 @@ def register():
 
     if form.validate_on_submit():
         password = generate_password_hash(form.password.data)
-        print(len(password))
+        # print(len(password))
         new_user = User(username=form.username.data, password=password)
         try:
             db.session.add(new_user)
             db.session.commit()
-            return "register successfully"
+            return redirect(url_for('login'))
         except:
             return "Error while add user"
 
@@ -107,12 +102,31 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user and check_password_hash(user.password, form.password.data):
-            return 'login success'
-        flash('Invalid login, Please try again')
+            session["user"] = user.username
+            # flash("login success")
+            return redirect(url_for('dashboard'))
+        flash('Invalid login, Please try again', "invalid_login")
         return redirect(url_for('login'))
-
+    
+    if "user" in session:
+        return redirect(url_for('dashboard'))
+    
     return render_template("login.html", form=form)
 
+
+@app.route('/dashboard')
+def dashboard():
+    if "user" in session:
+        username = session["user"]
+        return render_template('dashboard.html', username=username)
+    
+    return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session.pop("user", None)
+    flash("Logout Successfully", "logout")
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True)
